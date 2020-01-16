690f0682809e25d4a87e93f3793799c42bff4b7e
Glitch (hello-webpage) <none>
2020-01-16 21:15:30 +0000

    🌷🚠 Checkpoint ./index.html:9162534/155 ./README.md:9162534/108 ./script.js:9162534/2029

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-mobilenet.load().then(function (model) {
-  // Classify the image.
-  model.classify(img).then(function (predictions) {
-    console.log('Predictions: ');
-    console.log(predictions);
-  });
+/********************************************************************
+// Created by Jason Mayes 2020.
+// Got questions? Reach out to me on social:
+// Twitter: @jason_mayes
+// LinkedIn: https://www.linkedin.com/in/creativetech
+********************************************************************/
+
+var modelHasLoaded = false;
+var model = undefined;
+
+// Before we can use MobileNet we must wait for it to finish loading. 
+// Machine Learning models can be large and take a moment to get 
+// everything they need to run.
+mobilenet.load().then(function (loadedModel) {
+  model = loadedModel;
+  modelHasLoaded = true;
+});
+
+
+
+model.classify(img).then(function (predictions) {
+  console.log('Predictions: ');
+  console.log(predictions);
 });
