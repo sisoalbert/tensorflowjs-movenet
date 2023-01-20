8eee20266167cd10cf93b8a9e348330ae223a061
Glitch (tensorflow-js-object-detection) <none>
2023-01-20 05:11:36 +0200

    Added the movenet model

--- script.js
+++ script.js
@@ -1,196 +1,143 @@
-/**
- * @license
- * Copyright 2018 Google LLC. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * =============================================================================
- */
+//1. Loading the raw pre-trained model
 
-/********************************************************************
- * Demo created by Jason Mayes 2020.
- *
- * Got questions? Reach out to me on social:
- * Twitter: @jason_mayes
- * LinkedIn: https://www.linkedin.com/in/creativetech
- ********************************************************************/
+const MODEL_PATH =
+  "https://tfhub.dev/google/tfjs-model/movenet/singlepose/lightning/4";
+const EXAMPLE_IMG = document.getElementById("exampleImg");
+// Get the canvas element
 
-const demosSection = document.getElementById('demos');
+let movenet = undefined;
 
-var model = undefined;
+async function loadAndRunModel() {
+  movenet = await tf.loadGraphModel(MODEL_PATH, { fromTFHub: true });
 
-// Before we can use COCO-SSD class we must wait for it to finish
-// loading. Machine Learning models can be large and take a moment to
-// get everything needed to run.
-cocoSsd.load().then(function (loadedModel) {
-  model = loadedModel;
-  // Show demo section now model is ready to use.
-  demosSection.classList.remove('invisible');
-});
+  // 2. Test: Before we use the model on real images that require processing, it is a good practice to use a dummy tensor with the required input shape to check whether the model is working correctly in your code. In this particular example, we create a tensor filled with zeros by using ‘tf.zeros’.
+  let exampleInputTensor = tf.zeros([1, 192, 192, 3], "int32");
 
+  // The tf.browser.fromPixels() function in TensorFlow.js is used to create a tensor from an image object. The image object can be an HTMLImageElement, an HTMLCanvasElement, or an HTMLVideoElement. This function will create a tensor with the shape of [numrows, numcolumns, numchannels], where numchannels is the number of color channels in the image (e.g. 1 for grayscale, 3 for RGB).
+  let imageTensor = tf.browser.fromPixels(EXAMPLE_IMG);
+  console.log("imageTensor", imageTensor.shape);
 
-/********************************************************************
-// Demo 1: Grab a bunch of images from the page and classify them
-// upon click.
-********************************************************************/
+  // Calculate the coordinates of the crop area. You may choose to use the COCO-SSD model to draw an ideal bounding box around the object on which you want the model to perform inference.
+  let cropStartPoint = [15, 170, 0];
+  let cropSize = [345, 345, 3];
 
-// In this demo, we have put all our clickable images in divs with the 
-// CSS class 'classifyOnClick'. Lets get all the elements that have
-// this class.
-const imageContainers = document.getElementsByClassName('classifyOnClick');
+  // The tf.slice() function in TensorFlow.js is used to extract a slice of a tensor. The function takes three parameters: the input tensor, the start position (an array of integers), and the size (also an array of integers). The output tensor will contain the values of the input tensor from the start position to the end of the specified size.
+  let croppedTensor = tf.slice(imageTensor, cropStartPoint, cropSize);
 
-// Now let's go through all of these and add a click event listener.
-for (let i = 0; i < imageContainers.length; i++) {
-  // Add event listener to the child element whichis the img element.
-  imageContainers[i].children[0].addEventListener('click', handleClick);
-}
+  // The tf.image.resizeBilinear() function in TensorFlow.js is used to resize an image tensor. The function takes three parameters: the input tensor, the desired output size (an array of two integers), and an argument which specifies whether to preserve the aspect ratio of the image or not (the default is true). The output tensor will contain the resized image.For example, to resize an image tensor to a size of [192, 192]:
+  let resizedTensor = tf.image
+    .resizeBilinear(croppedTensor, [192, 192], true)
+    .toInt();
 
-// When an image is clicked, let's classify it and display results!
-function handleClick(event) {
-  if (!model) {
-    console.log('Wait for model to load before clicking!');
-    return;
-  }
-  
-  // We can call model.classify as many times as we like with
-  // different image data each time. This returns a promise
-  // which we wait to complete and then call a function to
-  // print out the results of the prediction.
-  model.detect(event.target).then(function (predictions) {
-    // Lets write the predictions to a new paragraph element and
-    // add it to the DOM.
-    console.log(predictions);
-    for (let n = 0; n < predictions.length; n++) {
-      // Description text
-      const p = document.createElement('p');
-      p.innerText = predictions[n].class  + ' - with ' 
-          + Math.round(parseFloat(predictions[n].score) * 100) 
-          + '% confidence.';
-      // Positioned at the top left of the bounding box.
-      // Height is whatever the text takes up.
-      // Width subtracts text padding in CSS so fits perfectly.
-      p.style = 'left: ' + predictions[n].bbox[0] + 'px;' + 
-          'top: ' + predictions[n].bbox[1] + 'px; ' + 
-          'width: ' + (predictions[n].bbox[2] - 10) + 'px;';
+  console.log("resizedTensorshape", resizedTensor.shape);
+  //Test below:
+  // let tensorOutput = movenet.predict(exampleInputTensor);
 
-      const highlighter = document.createElement('div');
-      highlighter.setAttribute('class', 'highlighter');
-      highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px;' +
-          'top: ' + predictions[n].bbox[1] + 'px;' +
-          'width: ' + predictions[n].bbox[2] + 'px;' +
-          'height: ' + predictions[n].bbox[3] + 'px;';
+  // Action
+  // use ‘tf.expandDims()’ to account for the additional dimension required to add the batch size to the tensor. Your image currently has the shape [192, 192, 3] while the shape of the expected input tensor is [1, 192, 192, 3]. Not doing this will cause a shape error.
+  let tensorOutput = movenet.predict(tf.expandDims(resizedTensor));
 
-      event.target.parentNode.appendChild(highlighter);
-      event.target.parentNode.appendChild(p);
-    }
-  });
-}
+  console.log("raw tensorOutput", tensorOutput);
 
+  //The tensorOutput.array() function returns the values of the tensor as a JavaScript array
+  let arrayOutput = await tensorOutput.array();
 
+  console.log("arrayOutput", arrayOutput);
 
-/********************************************************************
-// Demo 2: Continuously grab image from webcam stream and classify it.
-// Note: You must access the demo on https for this to work:
-// https://tensorflow-js-image-classification.glitch.me/
-********************************************************************/
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
 
-const video = document.getElementById('webcam');
-const liveView = document.getElementById('liveView');
+  // Draw the image on the canvas
+  ctx.drawImage(EXAMPLE_IMG, 0, 0);
 
-// Check if webcam access is supported.
-function hasGetUserMedia() {
-  return !!(navigator.mediaDevices &&
-    navigator.mediaDevices.getUserMedia);
-}
+  // Draw a small circular point at x: 170, y: 15 from the box
+  ctx.beginPath();
+  ctx.arc(170, 15, 3, 0, 2 * Math.PI);
+  ctx.fillStyle = "blue";
+  ctx.fill();
 
-// Keep a reference of all the child elements we create
-// so we can remove them easilly on each render.
-var children = [];
+  // Draw a small circular point at x: 94, y: 15 from the box
+  ctx.beginPath();
+  let x = 170 + 345 / 2;
+  let y = 15 + 345 / 2;
+  ctx.arc(x, y, 3, 0, 2 * Math.PI);
+  ctx.fillStyle = "green";
+  ctx.fill();
 
+  // Set the stroke style to red
+  ctx.strokeStyle = "red";
+  // Draw a box at x: 170, y: 15, width: 345, height: 345
+  ctx.strokeRect(170, 15, 345, 345);
+  // Draw a small circular point at x: 94, y: 15 from the box
+  ctx.beginPath();
+  ctx.arc(264, 30, 3, 0, 2 * Math.PI);
+  ctx.fillStyle = "red";
+  ctx.fill();
 
-// If webcam supported, add event listener to button for when user
-// wants to activate it.
-if (hasGetUserMedia()) {
-  const enableWebcamButton = document.getElementById('webcamButton');
-  enableWebcamButton.addEventListener('click', enableCam);
-} else {
-  console.warn('getUserMedia() is not supported by your browser');
-}
+  const inputSize = 192;
+  const arr = [
+    [
+      [
+        [0.08709511160850525, 0.49314209818840027, 0.5901185274124146],
+        [0.07062023133039474, 0.5149994492530823, 0.7489978075027466],
+        [0.07055883854627609, 0.4780544340610504, 0.5669557452201843],
+      ],
+    ],
+  ];
 
+  let coordinates = [];
 
-// Enable the live webcam view and start classification.
-function enableCam(event) {
-  if (!model) {
-    console.log('Wait! Model not loaded yet.')
-    return;
-  }
-  
-  // Hide the button.
-  event.target.classList.add('removed');  
-  
-  // getUsermedia parameters.
-  const constraints = {
-    video: true
-  };
-
-  // Activate the webcam stream.
-  navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-    video.srcObject = stream;
-    video.addEventListener('loadeddata', predictWebcam);
-  });
-}
-
-
-// Prediction loop!
-function predictWebcam() {
-  // Now let's start classifying the stream.
-  model.detect(video).then(function (predictions) {
-    // Remove any highlighting we did previous frame.
-    for (let i = 0; i < children.length; i++) {
-      liveView.removeChild(children[i]);
-    }
-    children.splice(0);
-    
-    // Now lets loop through predictions and draw them to the live view if
-    // they have a high confidence score.
-    for (let n = 0; n < predictions.length; n++) {
-      // If we are over 66% sure we are sure we classified it right, draw it!
-      if (predictions[n].score > 0.66) {
-        const p = document.createElement('p');
-        p.innerText = predictions[n].class  + ' - with ' 
-            + Math.round(parseFloat(predictions[n].score) * 100) 
-            + '% confidence.';
-        // Draw in top left of bounding box outline.
-        p.style = 'left: ' + predictions[n].bbox[0] + 'px;' +
-            'top: ' + predictions[n].bbox[1] + 'px;' + 
-            'width: ' + (predictions[n].bbox[2] - 10) + 'px;';
-
-        // Draw the actual bounding box.
-        const highlighter = document.createElement('div');
-        highlighter.setAttribute('class', 'highlighter');
-        highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px; top: '
-            + predictions[n].bbox[1] + 'px; width: ' 
-            + predictions[n].bbox[2] + 'px; height: '
-            + predictions[n].bbox[3] + 'px;';
-
-        liveView.appendChild(highlighter);
-        liveView.appendChild(p);
-        
-        // Store drawn objects in memory so we can delete them next time around.
-        children.push(highlighter);
-        children.push(p);
+  for (let i = 0; i < arrayOutput.length; i++) {
+    for (let j = 0; j < arrayOutput[i].length; j++) {
+      for (let k = 0; k < arrayOutput[i][j].length; k++) {
+        coordinates.push([
+          arrayOutput[i][j][k][0] * inputSize,
+          arrayOutput[i][j][k][1] * inputSize,
+        ]);
       }
     }
-    
-    // Call this function again to keep predicting when the browser is ready.
-    window.requestAnimationFrame(predictWebcam);
-  });
+  }
+  console.log(coordinates);
+
+  const Xd = 170;
+  const Yd = 15;
+
+  let newArr = [];
+  for (let i = 0; i < coordinates.length; i++) {
+    console.log(coordinates[i][0] + Yd);
+    console.log(coordinates[i][1] + Xd);
+
+    newArr.push([coordinates[i][1] + Xd, coordinates[i][0] + Yd]);
+  }
+  console.log(newArr);
+
+  // Draw small circles at newArr points
+  for (let i = 0; i < newArr.length; i++) {
+    ctx.beginPath();
+    ctx.arc(newArr[i][0], newArr[i][1], 2, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  // // Draw a small circular point at x: 94, y: 15
+  // ctx.beginPath();
+  // ctx.arc(640, 15, 3, 0, 5 * Math.PI);
+  // ctx.fillStyle = "red";
+  // ctx.fill();
+
+  // const keypoints = arrayOutput;
+
+  // // Draw rectangles for the keypoints
+  // for (let i = 0; i < keypoints[0].length; i++) {
+  //   const keypoint = keypoints[0][i];
+  //   if (keypoint[2] > 0.5) {
+  //     ctx.fillRect(
+  //       keypoint[0] * canvas.width,
+  //       keypoint[1] * canvas.height,
+  //       5,
+  //       5
+  //     );
+  //   }
+  // }
 }
+
+loadAndRunModel();
